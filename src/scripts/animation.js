5dee6ae964754a4dc231cd5f1778b2c2c7aec0e5
kubaejj <jakub.jezbera.023@ pslib.cz>
2025-05-23 13:06:21 +0200

    test %base%

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -3,7 +3,7 @@ const totalSteps = window.innerWidth / 40; // Počet obrázků
 
 for (let i = 0; i < totalSteps; i++) {
   const img = document.createElement('img');
-  img.src = '/img/animation/footstep.svg';
+  img.src = '%BASE_URL%/img/animation/footstep.svg';
   img.className = 'footstep';
   img.style.setProperty('--i', i);
   img.alt = 'stopa';
