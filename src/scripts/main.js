4050b0b7fa1dde33f5098cb8cd5a8a9db5a5b0a0
kubaejj <jakub.jezbera.023@pslib.cz>
2025-05-23 22:25:00 +0200

    puvodni stav - private verze

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -58,6 +58,8 @@ import '../styles/fonts.css';
 import '../styles/icons.css';
 import '../styles/hamburger.css';*/
 import '../styles/style.css';
+
+/*
 import Clarity from '@microsoft/clarity';
 const projectId = "rnzf8513xv"
-Clarity.init(projectId);
+Clarity.init(projectId);*/
