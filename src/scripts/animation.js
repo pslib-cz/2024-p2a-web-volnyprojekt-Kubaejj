2bf6ea23c440357bc8d39cc9cf0fbe222ea39526
kubaejj <jakub.jezbera.023@pslib.cz>
2025-05-23 18:31:24 +0200

    pridani %baseurl% k podvodum

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -4,7 +4,7 @@ const totalSteps = window.innerWidth / 40;
 for (let i = 0; i < totalSteps; i++) {
   const img = document.createElement('img');
 //  img.src = '/img/animation/footstep.svg';
-  img.src = '%BASE_URL%img/animation/footstep.svg';
+ // img.src = '%BASE_URL%img/animation/footstep.svg';
   img.className = 'footstep';
   img.style.setProperty('--i', i);
   img.alt = 'stopa';
