f19a0e188e823adcc4d5ae93aab21e7755af6e45
kubaejj <jakub.jezbera.023@pslib.cz>
2025-05-23 18:17:21 +0200

    %base v indexu na obrazky? funguej snad

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -3,8 +3,8 @@ const totalSteps = window.innerWidth / 40;
 
 for (let i = 0; i < totalSteps; i++) {
   const img = document.createElement('img');
-  img.src = '/img/animation/footstep.svg';
- // img.src = '%BASE_URL%img/animation/footstep.svg'; - uri error
+//  img.src = '/img/animation/footstep.svg';
+  img.src = '%BASE_URL%img/animation/footstep.svg';
   img.className = 'footstep';
   img.style.setProperty('--i', i);
   img.alt = 'stopa';
