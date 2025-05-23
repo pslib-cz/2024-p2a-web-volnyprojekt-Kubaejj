bcec28fea7b5f9394f401aed70fc8db25d030956
kubaejj <jakub.jezbera.023@pslib.cz>
2025-05-23 18:02:14 +0200

    test s absoluitni cestou gh pages

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,9 +1,10 @@
 const footstepDiv = document.getElementById('footsteps-container');
-const totalSteps = window.innerWidth / 40; // Počet obrázků
+const totalSteps = window.innerWidth / 40;
 
 for (let i = 0; i < totalSteps; i++) {
   const img = document.createElement('img');
-  img.src = '%BASE_URL%/img/animation/footstep.svg';
+  img.src = '/img/animation/footstep.svg';
+ // img.src = '%BASE_URL%img/animation/footstep.svg'; - uri error
   img.className = 'footstep';
   img.style.setProperty('--i', i);
   img.alt = 'stopa';
